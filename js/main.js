749600861a26ecc025f1728808c962d979f1c685
woody <45289944+woody21978@users.noreply.github.com>
2020-06-28 19:20:56 +0300

    Закончил верстку

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  $('.teams__list').slick({
+    centerPadding: '42px',
+    autoplay: true,
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '555px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
